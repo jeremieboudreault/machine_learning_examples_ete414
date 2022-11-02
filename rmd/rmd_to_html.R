2abce892019957b367e320e61ef0759e72345d56
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-11-02 15:13:34 -0400

    Added all Rmd documents to the repo.

---
+++ rmd/rmd_to_html.R
@@ -0,0 +1,5 @@
+# Render Rmarkdown document to html.
+rmarkdown::render(
+    input       = "rmd/exemples_R_arbres_forets_boosting.Rmd",
+    output_file = "exemples_R_arbres_forets_boosting.html"
+)
