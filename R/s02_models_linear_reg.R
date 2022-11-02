52bde1a40742946fc68f0ac7076a5dff05ccaf26
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2022-11-02 13:42:31 -0400

    Fitted linear models on train, and tested on test. Produced the figure using jtheme.

--- R/s02_models_linear_reg.R
+++ R/s02_models_linear_reg.R
@@ -7,23 +7,23 @@
 # Project : water_temperature_neural_networkds
 # Author  : Jeremie Boudreault
 # Email   : Jeremie.Boudreault [at] inrs [dot] ca
-# Depends : R (v3.6.3)
+# Depends : R (v4.2.1)
+# Imports : jtheme v0.0.3 [https://github.com/jeremieboudreault/jtheme]
 # License : CC BY-NC-ND 4.0
-
 
 
 # Library ----------------------------------------------------------------------
 
 
 library(data.table)
-library(ggplot2)
+library(jtheme)
 
 
 # Imports ----------------------------------------------------------------------
 
 
 data <- data.table::fread(
-    file.path("data", "cleaned", "hydro_weather_data_v1.csv")
+    file.path("data", "cleaned", "hydro_weather_data_v2.csv")
 )
 
 
@@ -31,18 +31,18 @@ data <- data.table::fread(
 
 
 fit_1 <- lm(
-    formula = WATERTEMP_MEAN ~ AIRTEMP_MEAN,
-    data    = data
+    formula = WATERTEMP ~ AIRTEMP,
+    data    = data[DATASET == "TRAIN", ]
 )
 
 fit_2 <- lm(
-    formula = WATERTEMP_MEAN ~ poly(AIRTEMP_MEAN, 2L),
-    data    = data
+    formula = WATERTEMP ~ poly(AIRTEMP, 2L),
+    data    = data[DATASET == "TRAIN", ]
 )
 
 fit_3 <- lm(
-    formula = WATERTEMP_MEAN ~ poly(AIRTEMP_MEAN, 3L),
-    data    = data
+    formula = WATERTEMP ~ poly(AIRTEMP, 3L),
+    data    = data[DATASET == "TRAIN", ]
 )
 
 
@@ -65,9 +65,15 @@ AIC(fit_3)
 # Compute RMSE -----------------------------------------------------------------
 
 
+# Train.
 RMSE_1 <- sqrt(mean(residuals(fit_1)^2))
 RMSE_2 <- sqrt(mean(residuals(fit_2)^2))
 RMSE_3 <- sqrt(mean(residuals(fit_3)^2))
+
+# Test
+RMSE_1_test <- sqrt(mean((predict(fit_1, data[DATASET == "TEST"]) - data[DATASET == "TEST", WATERTEMP])^2))
+RMSE_2_test <- sqrt(mean((predict(fit_2, data[DATASET == "TEST"]) - data[DATASET == "TEST", WATERTEMP])^2))
+RMSE_3_test <- sqrt(mean((predict(fit_3, data[DATASET == "TEST"]) - data[DATASET == "TEST", WATERTEMP])^2))
 
 
 # Extract regression lines -----------------------------------------------------
@@ -75,7 +81,7 @@ RMSE_3 <- sqrt(mean(residuals(fit_3)^2))
 
 # Create x grid.
 reg_lines <- data.table::data.table(
-    AIRTEMP_MEAN = seq(min(data$AIRTEMP_MEAN), max(data$AIRTEMP_MEAN), length.out = 100)
+    AIRTEMP = seq(min(data$AIRTEMP), max(data$AIRTEMP), length.out = 100)
 )
 
 # Predict on the grid.
@@ -84,15 +90,15 @@ reg_lines[, Degree2 := predict(fit_2, newdata = reg_lines)]
 reg_lines[, Degree3 := predict(fit_3, newdata = reg_lines)]
 
 # Melt table.
-reg_lines <- data.table::melt.data.table(reg_lines, id.vars = "AIRTEMP_MEAN")
+reg_lines <- data.table::melt.data.table(reg_lines, id.vars = "AIRTEMP")
 
 # Rename columns.
 reg_lines[variable == "Linear",
-          variable := paste0("Linear (RMSE = ", round(RMSE_1, 2L), "ºC)")]
+          variable := paste0("Linear (RMSE = ", round_trim(RMSE_1, 2L), "/", round_trim(RMSE_1_test, 2L), "ºC)")]
 reg_lines[variable == "Degree2",
-          variable := paste0("Degree 2 (RMSE = ", round(RMSE_2,2L), "ºC)")]
+          variable := paste0("Degree 2 (RMSE = ", round_trim(RMSE_2,2L), "/", round_trim(RMSE_2_test, 2L), "ºC)")]
 reg_lines[variable == "Degree3",
-          variable := paste0("Degree 3 (RMSE = ", round(RMSE_3, 2L), "ºC)")]
+          variable := paste0("Degree 3 (RMSE = ", round_trim(RMSE_3, 2L), "/", round_trim(RMSE_3_test, 2L), "ºC)")]
 
 
 # Plot final results -----------------------------------------------------------
@@ -101,8 +107,8 @@ reg_lines[variable == "Degree3",
 ggplot(
     data   = data,
     mapping = aes(
-        x = AIRTEMP_MEAN,
-        y = WATERTEMP_MEAN
+        x = AIRTEMP,
+        y = WATERTEMP
     )
 ) +
 geom_point(
@@ -114,27 +120,27 @@ geom_point(
 geom_line(
     data = reg_lines,
     mapping = aes(
-        x = AIRTEMP_MEAN,
-        y = value,
+        x   = AIRTEMP,
+        y   = value,
         col = variable
     ),
     lwd = 0.7
 ) +
 labs(
-    title = "Regression models for mean water temperature",
+    title = "Linear models for water temperature",
     x     = "Air temperature (ºC)",
     y     = "Water temperature (ºC)",
-    color = "Models"
+    color = ""
 ) +
-theme(legend.position = c(0.825, 0.175))
+jtheme(border = "all") +
+theme(legend.position = c(0.835, 0.125))
 
 
 # Exports -----------------------------------------------------------------
 
 
-ggplot2::ggsave(
-    filename = file.path("plots", "fig_01_linear_models_results.jpg"),
-    width    = 7L,
-    height   = 5L,
+jtheme::save_ggplot(
+    file = file.path("plots", "fig_01_linear_models_results.jpg"),
+    size = "rect"
 )
 
